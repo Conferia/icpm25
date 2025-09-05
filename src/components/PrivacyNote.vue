5e395810fed3440a70cb726172fd7e45a05c8b2e
Andrea Burattin <andrea.burattin@gmail.com>
2025-09-05 15:35:28 +0200

    Fixed privacy text

--- src/components/PrivacyNote.vue
+++ src/components/PrivacyNote.vue
@@ -11,22 +11,22 @@
     <ion-content class="ion-padding">
 
       <h4>How we process your personal data</h4>
-      <p>DTU, who is the organizer of ICPM 2024, as data controller, collects and processes information about you in connection with your registration and the partecipation to the conference.</p>
+      <p>CONFERENCE-ORGANIZER, who is the organizer of CONFERENCE-NAME, as data controller, collects and processes information about you in connection with your registration and the partecipation to the conference.</p>
       <p>We process your information on the basis of Article 6(1)(b) and (e) of the Regulation and, in some cases, on the basis of your consent in accordance with Article 6(1)(a) of the Regulation.</p>
 
       <h4>Type of data</h4>
-      <p>By registering for the ICPM Conference, you are added to the participant (delegates) list, which is shown in the ICPM App.</p>
+      <p>By registering for the CONFERENCE-NAME, you are added to the participant (delegates) list, which is shown in this application.</p>
       <p>We collect and process the following data:</p>
       <ul>
-        <li>Name: Visible to all the participants using the ICPM App.</li>
-        <li>Messages and pictures that you voluntarily upload to the ICPM App: Visible to all the participants using the ICPM App.</li>
-        <li>Affiliation, country, e-mail, and profile picture: Visible to all the participants using the ICPM App only if you actively decide to share them.</li>
+        <li>Name: Visible to all the participants using this application.</li>
+        <li>Messages and pictures that you voluntarily upload to the application: Visible to all the participants using this application.</li>
+        <li>Affiliation, country, e-mail, and profile picture: Visible to all the participants using this application only if you actively decide to share them.</li>
       </ul>
-      <p>We do not process sensitive personal data in the ICPM App. Please do not share sensitive data via the ICPM App.</p>
-      <p>Your data will not be disclosed by DTU to third parties unless we are obliged to provide the data because of a legal obligation or in order to perform a task in the public interest. DTU does not transfer your data to countries outside the European Union.</p>
+      <p>We do not process sensitive personal data in this application. Please do not share sensitive data via this application.</p>
+      <p>Your data will not be disclosed by CONFERENCE-ORGANIZER to third parties unless we are obliged to provide the data because of a legal obligation or in order to perform a task in the public interest. CONFERENCE-ORGANIZER does not transfer your data to countries outside the European Union.</p>
 
       <h4>Storage and disclosure</h4>
-      <p>We store your data on DTU servers for as long as necessary for the purpose for which the data is processed. When it is no longer relevant to store your data in order to fulfill its purpose, the data will be deleted or archived. This will happen no later than one year after the conference.</p>
+      <p>We store your data on CONFERENCE-ORGANIZER servers for as long as necessary for the purpose for which the data is processed. When it is no longer relevant to store your data in order to fulfill its purpose, the data will be deleted or archived. This will happen no later than one year after the conference.</p>
 
       <h4>Your rights</h4>
       <p>When we process data about you, as the data subject, you have a number of rights:</p>
@@ -43,16 +43,14 @@
       <h4>Legal entity responsible for the data processing</h4>
 
       <p>
-      Danmarks Tekniske Universitet<br>
-      DTU Compute<br>
-      Richard Petersens Plads<br>
-      Building 324<br>
-      DK-2800 Kgs. Lyngby<br>
-      E-mail: compute@compute.dtu.dk
+		CONFERENCE-ORGANIZER<br>
+		Address 1<br>
+		Address 2<br>
+		Country<br>
       </p>
 
-      <h4>Contact details of DTU's Data Protection Officer</h4>
-      <p>DTU's DPO: dpo@dtu.dk</p>
+      <h4>Contact details of CONFERENCE-ORGANIZER's Data Protection Officer</h4>
+      <p>CONFERENCE-ORGANIZER's DPO: EMAIL-OF-DPO</p>
     </ion-content>
   </ion-modal>
  </template>
