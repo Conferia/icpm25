f11c5d2b008a0f8aff6636c5553b05f15806d104
Leonel Peña <lapo26@gmail.com>
2025-10-10 20:59:07 -0300

    Update TabHome.vue

--- src/views/TabHome.vue
+++ src/views/TabHome.vue
@@ -3,7 +3,7 @@
     <HeaderBar name="Home" />
 
     <ion-content id="main-content" :fullscreen="true">
-      <img :src="backend.config.homePagePicture" width="100%" alt=""/>
+      <img :src="backend.config.homePagePicture" width="100%" alt="" />
 
       <ion-card>
         <ion-card-header>
@@ -11,9 +11,9 @@
           <ion-card-title>ICPM 2025</ion-card-title>
         </ion-card-header>
         <ion-card-content>
-			7<sup>th</sup> International Conference on Process Mining<br><br>
-			October 20-24, 2025<br>
-			Montevideo, Uruguay
+          7<sup>th</sup> International Conference on Process Mining<br /><br />
+          October 20-24, 2025<br />
+          Montevideo, Uruguay
         </ion-card-content>
       </ion-card>
       <p class="ion-padding" style="font-size: 1.5rem; margin-bottom: 0">
@@ -21,16 +21,16 @@
       </p>
       <ion-list lines="full">
         <ion-item
-            button
-            v-for="page in pages"
-            :key="page.id"
-            :routerLink="`/tabs/page/${page.id}`"
-            @click="() => trackButtonClick('Navigate to Page', 'Home Page', 'Feature')">
-          <ion-label >{{ page.name }}</ion-label>
+          button
+          v-for="page in pages"
+          :key="page.id"
+          :routerLink="`/tabs/page/${page.id}`"
+          @click="() => trackButtonClick('Navigate to Page', 'Home Page', 'Feature')"
+        >
+          <ion-label>{{ page.name }}</ion-label>
           <ion-badge v-if="page.label" color="danger">{{ page.label }}</ion-badge>
         </ion-item>
       </ion-list>
-<!--      <InstallApplication />-->
     </ion-content>
   </ion-page>
 </template>
@@ -47,22 +47,37 @@ import {
   IonList,
   IonItem,
   IonLabel,
-  menuController, IonToast
+  menuController,
+  IonToast,
 } from '@ionic/vue';
-import HeaderBar from "@/components/HeaderBar.vue";
-import { onMounted, reactive } from 'vue';
+import HeaderBar from '@/components/HeaderBar.vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 import axios from 'axios';
-import {onBeforeRouteLeave} from "vue-router";
-import backend from "/backend.config.ts";
-import {googleanalytics} from "@/composables/googleanalytics.ts";
+import { onBeforeRouteLeave } from 'vue-router';
+import backend from '/backend.config.ts';
+import { googleanalytics } from '@/composables/googleanalytics.ts';
 
-const{trackButtonClick} = googleanalytics()
+const { trackButtonClick } = googleanalytics();
 
 const pages = reactive([]);
-const token = localStorage.getItem("accessToken")
-const logo = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? backend.config.logoDark
-      : backend.config.logoLight;
+const token = localStorage.getItem('accessToken');
+const isDarkMode = ref(
+  window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+);
+const logo = ref(
+  isDarkMode.value ? backend.config.logoDark : backend.config.logoLight
+);
+
+if (window.matchMedia) {
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  darkModeMediaQuery.addEventListener('change', (event) => {
+    isDarkMode.value = event.matches;
+  });
+}
+
+watch(isDarkMode, (newValue) => {
+  logo.value = newValue ? backend.config.logoDark : backend.config.logoLight;
+});
 
 const closeSettingsMenu = async () => {
   await menuController.close('settings-menu');
@@ -74,22 +89,34 @@ onBeforeRouteLeave((to, from) => {
 
 onMounted(async () => {
   try {
-    const response = await axios.get(backend.construct( 'pages'),{ headers: { Authorization: `Bearer ${token}` } });
-    pages.splice(0, pages.length, ...response.data.map(page => ({
+    const response = await axios.get(backend.construct('pages'), {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    pages.splice(0, pages.length, ...response.data.map((page) => ({
       id: page.id,
       name: page.title,
-
     })));
   } catch (error) {
-  console.error('Failed to fetch pages', error);
+    console.error('Failed to fetch pages', error);
   }
 });
 </script>
 
-<style scoped>
+<style>
+body:not(.dark) #logo-large {
+  background-image: var(--logo-light);
+}
+
+body.dark #logo-large {
+  background-image: var(--logo-dark);
+}
+
 #logo-large {
-  /* height: 50px; */
-  width: 250px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 50px;
+  width: 100%;
   margin-bottom: 10px;
 }
 </style>
